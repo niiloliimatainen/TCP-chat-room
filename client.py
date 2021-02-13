5a9396815b62121475d4a44f4f6b71d264fc7d49
niiloliimatainen <niilo.liimatainen@student.lut.fi>
2021-02-13 20:59:20 +0200

    Version 1.1

--- client.py
+++ client.py
@@ -7,6 +7,7 @@
 #########################################
 import socket
 import threading
+import time
 
 # host address and port number (local)
 hostname = "127.0.0.1"
@@ -22,20 +23,39 @@ def receive():
     while True:
         try:
             msg = s.recv(1024).decode("utf-8")
-            if msg == "nickname":
+            if msg == "NICK":
                 s.send(nickname.encode("utf-8"))
+            elif msg == "PRIV":
+                s.send(input("Give nickname for private message: ").encode("utf-8"))
+                msg = input("Write private message: ")
+            elif msg == "PRIV-MSG":
+                time = get_time()
+                s.send(time + " " + nickname + msg.encode("utf-8"))
             else:
                 print(msg)
         except:
-            print("Error in connection!")
+            print("You have left the chat!")
             s.close()
             break
 
 
 def send():
     while True:
-        msg = f"{nickname} {input('')}"
-        s.send(msg.encode("utf-8"))
+        msg = input("")
+        if msg == "/exit":
+            s.send(msg.encode("utf-8"))
+        elif msg == "/private":
+            s.send(msg.encode("utf-8"))
+        else:
+            time = get_time()
+            msg = time + " " + nickname + ": " + msg
+            s.send(msg.encode("utf-8"))
+
+
+def get_time():
+    t = time.localtime()
+    current = time.strftime("%H:%M:%S", t)
+    return current
 
 
 # Threads for listening new messages and sending new messages
--- server.py
+++ server.py
@@ -27,20 +27,42 @@ def broadcast(msg):
         c.send(msg)
 
 
+def private_message(s_client, client_recv):
+    s_client.send("PRIV-MSG".encode("utf-8"))
+    msg = s_client.recv(1024)
+    client_recv.send(msg)
+
+
+# Function to kick client from the server
+def kick(s_client):
+    index = clients.index(s_client)
+    nickname = nicknames[index]
+    nicknames.remove(nickname)
+    clients.remove(s_client)
+    s_client.close()
+    print(f"{nickname} has left the server!")
+    broadcast(f"{nickname} has left the chat!".encode("utf-8"))
+
+
 # Function to listen to client's messages. If there is an error in connection, the client is removed from the server.
-def client_handler(client):
+def client_handler(s_client):
     while True:
         try:
-            msg = client.recv(1024)
-            broadcast(msg)
+            msg = s_client.recv(1024).decode("utf-8")
+            print(msg)
+            if msg == "/exit":
+                kick(s_client)
+                break
+            elif msg == "/private":
+                s_client.send("PRIV".encode("utf-8"))
+                nickname = s_client.recv(1024).decode("utf-8")
+                index = nicknames.index(nickname)
+                client_recv = clients[index]
+                private_message(s_client, client_recv)
+            else:
+                broadcast(msg.encode("utf-8"))
         except:
-            index = clients.index(client)
-            nickname = nicknames[index]
-            nicknames.remove(nickname)
-            clients.remove(client)
-            client.close()
-            print(f"{address} {nickname} has left the server!")
-            broadcast(f"{nickname} has left the chat!".encode("utf-8"))
+            kick(s_client)
             break
 
 
@@ -51,13 +73,16 @@ def receive():
     while True:
         s_client, address = s.accept()
 
-        s_client.send("nickname".encode("utf-8"))
+        s_client.send("NICK".encode("utf-8"))
         nickname = s_client.recv(1024).decode("utf-8")
         clients.append(s_client)
         nicknames.append(nickname)
 
         print(f"{address} connected to the server with nickname: {nickname}")
         s_client.send("You are connected to the server!".encode("utf-8"))
+        s_client.send("Here are commands that you can use:\n".encode("utf-8"))
+        s_client.send(
+            "/exit = leave server, /room1 = go to room 1, /room2 = go to room 2, /private = private message to user\n".encode("utf-8"))
         broadcast(f"{nickname} joined the chat!".encode("utf-8"))
 
         thread = threading.Thread(target=client_handler, args=(s_client,))
